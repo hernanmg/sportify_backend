2f2b5cb06fdc6f85ac6d9c8e9272c2f6dfa6405c
hernan <hernan.milers@fligoo.com>
2024-11-08 10:42:39 -0300

    creacion app main

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,15 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  async validateUser(profile: any): Promise<any> {
+    console.log('Validando el usuario con el perfil:', profile); // Verifica que el perfil esté llegando
+    const user = {
+      email: profile.emails[0].value,
+      name: profile.displayName,
+      googleId: profile.id,
+    };
+    // Aquí deberías validar el usuario en tu base de datos o crear uno nuevo
+    return user;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.enableCors({
+  //   origin: 'http://localhost:3000', // Ajusta esto según tu frontend
+  //   credentials: true,
+  // });
+
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Sportify Amateur')
+    .setDescription('Documentación de la API de ejemplo con Swagger')
+    .setVersion('1.0')
+    // .addBearerAuth() // Agrega esta línea si tienes autenticación con Bearer token
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
